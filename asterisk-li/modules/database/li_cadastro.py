9433cd898e259476c3e5fc6329ef6c1bf62ef7d5
jessica.hahn <jessica.hahn.silva@gmail.com>
2020-03-01 23:32:12 -0300

    ADD: interface de cadastro de li

---
+++ asterisk-li/modules/database/li_cadastro.py
@@ -0,0 +1,50 @@
+#!/opt/li-asterisk/tools/Python-3.6.7
+
+from oficio import Oficio
+
+class LiCadastro(Oficio):
+    def __init__(self, liid, target, uri, number, autority, date, database, log):
+        super().__init__(number, autority, date, database, log)
+        self.liid = liid
+        self.target = target
+        self.db = database
+        self.log = log
+        self.number = number
+
+    def register(self):
+        self.log.info("LiCadastro::register: Trying register Lawful Interception")
+        oficio = None
+        super().register()
+        oficio = super().get_oficio(self.number)
+        uri = self.get_uri(target)
+        sql = '''INSERT INTO li_cadastro(liid,target,uri,numero_oficio)
+            VALUES(?,?,?,?) '''
+        
+        self.db.connect()
+
+        parameters = (self.liid,self.target,uri,oficio)
+        self.log.info("LiCadastro::register: Parameters: " + str(parameters))
+        (cursor,conn) = self.db.execute_query(sql,parameters)
+        conn.commit()
+        self.log.info("LiCadastro::register: Lawful Interception registered")
+
+    def get_oficios(self):
+        self.log.info("LiCadastro::register: Trying get all LI")
+        li = []
+        sql = '''select * from li_cadastro'''
+        self.db.connect()
+        (cursor,conn) = self.db.execute_query(sql)
+        li = cursor.fetchall()
+        self.log.info("LiCadastro::register: li: " + str(li))
+        return oficios
+
+    def get_uri(self, cpf):
+        self.log.info("LiCadastro::get_uri: Trying get uri with cpf: " + str(cpf))
+        uri = None
+        sql = '''select uri from operadora where cpf = ?'''
+        self.db.connect()
+        parameters = (cpf)
+        (cursor,conn) = self.db.execute_query(sql, parameters)
+        uri = cursor.fetchone()
+        self.log.info("LiCadastro::get_uri: URI: " + str(uri))
+        return uri
