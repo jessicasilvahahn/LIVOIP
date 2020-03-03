113a03578315dbd5418eefa9d839052e96c24d44
jessica.hahn <jessica.hahn.silva@gmail.com>
2020-03-02 23:48:46 -0300

    MOD: insercao liid e instancia do database

--- asterisk-li/modules/user_interface/simple/interface.py
+++ asterisk-li/modules/user_interface/simple/interface.py
@@ -1,8 +1,10 @@
 #!/opt/li-asterisk/tools/Python-3.6.7
-
+from modules.database.li_cadastro import LiCadastro
+from modules.database import Database
 class Interface():
-    def __init__(self):
-        self.li_cadastro = None
+    def __init__(self, log, database_name):
+        self.log = log
+        self.db_name = database_name
 
     def li_register(self):
         print("######## Lawful Interception #########\n")
@@ -13,4 +15,9 @@ class Interface():
         date = input("Data da Interceptação\n")
         print("------- Alvo ------\n")
         target = input("CPF do alvo\n")
+        #calcular hash por em quanto
+        liid = hash(target)
+        database = Database(self.db_name, self.log)
+        li_cadastro = LiCadastro(liid, target, number, autority, date, database, self.log)
+        return li_cadastro
     
