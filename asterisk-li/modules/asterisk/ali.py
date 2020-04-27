e2131e3e03d418c371950dc379f9675709b34b2f
jessica.hahn <jessica.hahn.silva@gmail.com>
2020-04-26 23:21:53 -0300

    MOD: envia lista de interceptacoes vindo do banco para iri e cc

--- asterisk-li/modules/asterisk/ali.py
+++ asterisk-li/modules/asterisk/ali.py
@@ -54,10 +54,10 @@ class Ali(Database):
                 interceptions = self.get_targets()
                 self.log.info("Ali::get_interceptions: Interceptions list: " + str(interceptions))
                 if(interceptions):
-                    self.log.info("Ali::get_interceptions: Trying send interceptions to iri: " + str(interceptions))
                     self.setup()
-                    #self.client_iri.send_message(interceptions)
-                    #self.client_iri.send_message("ACK")
+                    self.log.info("Ali::get_interceptions: Trying send interceptions to iri: " + str(interceptions))
+                    self.client_iri.send_message(interceptions)
+                    self.client_iri.send_message("ACK")
                     self.log.info("Ali::get_interceptions: Trying send interceptions to record")
                     self.client_cc.connect()
                     self.client_cc.send_message(interceptions)
