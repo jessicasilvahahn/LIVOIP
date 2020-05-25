516e7ddbef5d92a5c22c7a377b65f1329e96d173
jessica.hahn <jessica.hahn.silva@gmail.com>
2020-05-24 21:23:44 -0300

    MOD: export ports container e mod databases

--- asterisk-li/scripts/run_docker.sh
+++ asterisk-li/scripts/run_docker.sh
@@ -3,4 +3,4 @@ path_container=$2
 docker_image=$3
 name=$4
 
-docker run -it --name=$name -v $path:$path_container:rw $docker_image bash
+docker run -it --name=$name -v $path:$path_container:rw --net=host -p 2403:2403 -p 2402:2402 -p 8081:8081 -p 8082:8082 $docker_image bash
